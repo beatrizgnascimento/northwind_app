9edf23b97a131a94c134b6c8f14a61e7abdb6b88
zinoLath <zinolath@gmail.com>
2025-04-17 18:39:42 -0300

    fix na orm

--- dao/dao_orm.py
+++ dao/dao_orm.py
@@ -2,9 +2,9 @@ from dao.dao_base import DAOBase
 import psycopg2 as psycopg
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
-from model.models import Orders, OrderDetails
+from model.models import Customers, Employees, Orders, OrderDetails, Employees
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 
 class DAOOrm(DAOBase):
     def inserir_order(self, customer_name, employee_name, order_date):
@@ -14,11 +14,11 @@ class DAOOrm(DAOBase):
 
         try:
             max_order_id = session.query(Orders).order_by(Orders.orderid.desc()).first()
-            customer = session.query(Orders).filter(Orders.contactname == customer_name).first()
+            customer = session.query(Customers).filter(Customers.contactname == customer_name).first()
             if not customer:
                 raise ValueError(f"Customer '{customer_name}' not found.")
             customer_id = customer.customerid
-            employee = session.query(Orders).filter(Orders.firstname == employee_name).first()
+            employee = session.query(Employees).filter(Employees.firstname == employee_name).first()
             if not employee:
                 raise ValueError(f"Employee '{employee_name}' not found.")
             employee_id = employee.employeeid
@@ -26,11 +26,11 @@ class DAOOrm(DAOBase):
             new_order = Orders(orderid=order_id, customerid=customer_id, employeeid=employee_id, orderdate=order_date)
             session.add(new_order)
             session.commit()
-            return True
+            return (True, "")
         except Exception as e:
             session.rollback()
             print(f"[ORM] Erro ao inserir pedido: {e}")
-            return False, e
+            return (False, e)
         finally:
             session.close()
 
@@ -52,10 +52,53 @@ class DAOOrm(DAOBase):
             return False, e
         finally:
             session.close()
-        pass
 
     def relatorio_order(self, order_id):
-        pass
+        engine = self.get_engine()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        try:
+            order = session.query(Orders).filter(Orders.orderid == order_id).first()
+            if not order:
+                raise ValueError(f"Pedido com ID '{order_id}' não encontrado.")
+            customer = session.query(Orders).filter(Orders.customerid == order.customerid).first()
+            employee = session.query(Orders).filter(Orders.employeeid == order.employeeid).first()
+            order_details = session.query(OrderDetails).filter(OrderDetails.orderid == order_id).all()
+            ret = f"ID do Pedido: {order.orderid}\n"
+            ret += f"Data do Pedido: {order.orderdate}\n"
+            ret += f"Nome do Cliente: {customer.contactname}\n"
+            ret += f"Nome do Vendedor: {employee.firstname}\n"
+            ret += "\n\nProdutos:\n"
+            for detail in order_details:
+                product = session.query(Orders).filter(Orders.productid == detail.productid).first()
+                ret += f"Produto: {product.productname}, Quantidade: {detail.quantity}, Preço Unitário: {detail.unitprice}, Desconto: {detail.discount}\n"
+
+        except Exception as e:
+            print(f"[ORM] Erro ao gerar relatório do pedido: {e}")
+            return False, e
+        finally:
+            session.close()
 
     def relatorio_employee(self, date_start, date_end):
-        pass
+        engine = self.get_engine()
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        try:
+            employees = session.query(Employees.firstname,func.sum(OrderDetails.quantity * OrderDetails.unitprice * (1 - OrderDetails.discount)))\
+            .join(Employees.orders).join(Orders.order_details).filter(Orders.orderdate.between(date_start,date_end)).\
+            group_by(Employees.firstname).order_by(func.sum(OrderDetails.quantity * OrderDetails.unitprice * (1 - OrderDetails.discount)).desc()).all()
+            #employees = session.query(Employees.firstname,).order_by(Employees.total_sales.desc()).all()
+            ret = "Relatório de Vendas por Funcionário:\n"
+            print(employees)
+            i = 1
+            for employee in employees:
+                ret += f"#{i} Vendedor: {employee[0]}, Total de Vendas: {employee[1]}\n"
+                i += 1
+            return ret
+        except Exception as e:
+            print(f"[ORM] Erro ao gerar relatório de funcionários: {e}")
+            return False, e
+        finally:
+            session.close()
